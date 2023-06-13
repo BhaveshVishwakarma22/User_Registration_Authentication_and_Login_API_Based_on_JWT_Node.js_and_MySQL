6073924838bb8669f3f6e3b93ac3bda739323bb8
BhaveshVishwakarma22 <bv200222@gmail.com>
2023-06-13 18:58:18 +0530

    Updated DotEnv

--- dbConnection/connectMySQL.js
+++ dbConnection/connectMySQL.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql')
 
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'Bhavesh@123',
-    database: 'testx'
+    host: process.env.DB_HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE
 })
 
 connection.connect((err)=>{
